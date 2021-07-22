8f1eda93596b98cf34aac661c6652ce04bd2a2c8
cloverspark <alexbrenna1999@hotmail.com>
2021-07-22 13:49:52 -0700

    gets information from form and adds to object

--- Controllers/AnimalController.cs
+++ Controllers/AnimalController.cs
@@ -3,13 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using ModelsAndModelBinding.Models;
 
 namespace ModelsAndModelBinding.Controllers
 {
     public class AnimalController : Controller
     {
+        [HttpGet]//when first go to url
         public IActionResult Add()
         {
+            return View();
+        }
+        [HttpPost]//called on submit
+        public IActionResult Add(IFormCollection form)
+        {
+            Animal pet = new Animal();
+            pet.AnimalName = form["Animal_Name"];
+            pet.OwnerFullName = form["Animals_Owner_Name"];
+            pet.breed = form["Animal_Breed"];
+            pet.DateTimeOfDropOff =Convert.ToDateTime(form["Date_Dropped_Off"]);
             return View();
         }
     }
