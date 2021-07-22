26fc71e5cc9a49a8c59c7ec7a57ebea76ec59940
cloverspark <alexbrenna1999@hotmail.com>
2021-07-22 14:48:54 -0700

    better display names

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,20 +18,24 @@ namespace ModelsAndModelBinding.Models
         /// <summary>
         /// Animals first name
         /// </summary>
+        [Display(Name = "Animal Name")] 
         public string AnimalName { get; set; }
         /// <summary>
         /// owner of animals legal
         /// first name and last name of owner
         /// </summary>
+        [Display(Name = "Owner's Full Name")]
         public string OwnerFullName { get; set; }
         /// <summary>
         /// the Breed of the animal 
         /// </summary>
+        [Display(Name = "Animal Breed")]
         public string Breed { get; set; }
         /// <summary>
         /// Day of animals getting dropped off at animal watcher(animal babysitter)
         /// Time is ignored
         /// </summary>
+        [Display(Name = "Drop off date of Animal")]
         public DateTime DateTimeOfDropOff { get; set; }
     }
 }
